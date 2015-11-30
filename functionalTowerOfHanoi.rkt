a932bc988bc894daa6734a5914cb45f232cf1fd4
Daniel Silva <silva10.daniel@gmail.com>
2015-11-30 19:26:10 -0200

    refatoracao. Nome de funcao, comentarios, tipo de retorno das funcoes

--- functionalTowerOfHanoi.rkt
+++ functionalTowerOfHanoi.rkt
@@ -3,50 +3,58 @@
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname functionalTowerOfHanoi) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ())))
 ;;developed by Daniel Silva
 ;
+;bibliotecas
 (require 2htdp/image)
 (require 2htdp/universe)
 (require racket/string)
 
- 
+;dimensões da cena
 (define WIDTH 600)
 (define HEIGHT 400)
 
 (define QTDE-DISCO 4)
 (define ALTURA-DISCO 15)
 
+;coordenadas para desenhar os pinos
 (define COORDENADA-Y-INICIAL-PINO 250)
 (define POSN-ORIGEM (make-posn 180 COORDENADA-Y-INICIAL-PINO))
 (define POSN-MEIO (make-posn 280 COORDENADA-Y-INICIAL-PINO))
 (define POSN-DESTINO (make-posn 380 COORDENADA-Y-INICIAL-PINO))
 
+;Dimensões do pino
 (define ALTURA-PINO 150)
 (define LARGURA-PINO 20)
 (define PINO (rectangle LARGURA-PINO ALTURA-PINO "solid" "blue"))
  
- 
+;diferença do disco 1 para o disco 2... 
 (define DIFERENCA-TAMANHO-DISCO 20)
 
 
-
+;estrutura de dados
 (define-struct world (solucao pinoOrigem pinoMeio pinoDestino))
 
-
+;world é um de:
+  ;(make-world string listaDeDiscos listaDeDiscos listaDeDiscos)
+  ;empty
 ;solucao e uma string com a solucao da torre de hanoi:
-; "pino1->pino2 pino2->3 ..."
+  ;formato:
+  ;"pino1->pino2 pino2->3 ..."
 
-;pinoOrigem armazena uma lista de discos. Ele é um de:
-;(cons (make-posn  tamanho-inicial ALTURA-DISCO) pinoOrigem)
-;empty
+;pinoOrigem armazena uma lista de discos. Ela é uma de:
+  ;(cons (make-posn  tamanho-inicial ALTURA-DISCO) pinoOrigem)
+  ;empty
 
-;pinoMeio armazena uma lista de discos. Ele é um de:
-;(cons (make-posn  tamanho-inicial ALTURA-DISCO) pinoMeio)
-;empty
+;pinoMeio armazena uma lista de discos. Ela é uma de:
+  ;(cons (make-posn  tamanho-inicial ALTURA-DISCO) pinoMeio)
+  ;empty
 
-;pinoDestino armazena uma lista de discos. Ele é um de:
-;(cons (make-posn  tamanho-inicial ALTURA-DISCO) pinoDestino)
-;empty
+;pinoDestino armazena uma lista de discos. Ela é uma de:
+  ;(cons (make-posn  tamanho-inicial ALTURA-DISCO) pinoDestino)
+  ;empty
 
 
+;desenha os pinos e os discos
+;é chamada toda vez que o timer estourar
 ;world -> image  
 (define (render w) 
   (cond
@@ -57,11 +65,14 @@
   )
 )
 
+;retorna a coordenada y inicial quer será usada para desenhar o disco da base no pino.
 ;none -> number
 (define (getCoordenadaYInicinal)
   ( - (+ COORDENADA-Y-INICIAL-PINO (/ ALTURA-PINO 2)) (/ ALTURA-DISCO  2))
  )
-;desenha os 3 pinos
+
+
+;desenha os 3 pinos e retorna uma cena que é usada para desenhar os discos
 ;none -> scene
 (define (desenharPino)
      
@@ -71,54 +82,60 @@
    
   
 )
+;move um disco do pino1 para o pino2
 ;world string string -> world
-(define (moverPino world pino1 pino2)
+(define (moverDisco  world pino1 pino2)
   (cond 
     [(string=? pino1 "origem")
       (cond 
-        [(string=? pino2 "meio") (tirarPino (make-world (world-solucao world) (world-pinoOrigem world) (colocarPino (world-pinoOrigem world) (world-pinoMeio world)) (world-pinoDestino world)) "origem")]
-        [(string=? pino2 "destino") (tirarPino (make-world (world-solucao world) (world-pinoOrigem world) (world-pinoMeio world)  (colocarPino (world-pinoOrigem world) (world-pinoDestino world))) "origem")]
+        [(string=? pino2 "meio") (tirarDisco(make-world (world-solucao world) (world-pinoOrigem world) (colocarDisco(world-pinoOrigem world) (world-pinoMeio world)) (world-pinoDestino world)) "origem")]
+        [(string=? pino2 "destino") (tirarDisco(make-world (world-solucao world) (world-pinoOrigem world) (world-pinoMeio world)  (colocarDisco(world-pinoOrigem world) (world-pinoDestino world))) "origem")]
       ) 
     ]  
     [(string=? pino1 "meio")
       (cond
-      [(string=? pino2 "origem") (tirarPino (make-world (world-solucao world) (colocarPino (world-pinoMeio world) (world-pinoOrigem world)) (world-pinoMeio world) (world-pinoDestino world)) "meio")]
-      [(string=? pino2 "destino") (tirarPino (make-world (world-solucao world) (world-pinoOrigem world) (world-pinoMeio world)  (colocarPino (world-pinoMeio world) (world-pinoDestino world))) "meio")]
+      [(string=? pino2 "origem") (tirarDisco(make-world (world-solucao world) (colocarDisco(world-pinoMeio world) (world-pinoOrigem world)) (world-pinoMeio world) (world-pinoDestino world)) "meio")]
+      [(string=? pino2 "destino") (tirarDisco(make-world (world-solucao world) (world-pinoOrigem world) (world-pinoMeio world)  (colocarDisco(world-pinoMeio world) (world-pinoDestino world))) "meio")]
       ) 
     ]
     [(string=? pino1 "destino")
       (cond
-        [(string=? pino2 "origem") (tirarPino (make-world (world-solucao world) (colocarPino (world-pinoDestino world) (world-pinoOrigem world)) (world-pinoMeio world) (world-pinoDestino world)) "destino")]
-        [(string=? pino2 "meio") (tirarPino (make-world  (world-solucao world) (world-pinoOrigem world) (colocarPino (world-pinoDestino world) (world-pinoMeio world)) (world-pinoDestino world)) "destino")]
+        [(string=? pino2 "origem") (tirarDisco(make-world (world-solucao world) (colocarDisco(world-pinoDestino world) (world-pinoOrigem world)) (world-pinoMeio world) (world-pinoDestino world)) "destino")]
+        [(string=? pino2 "meio") (tirarDisco(make-world  (world-solucao world) (world-pinoOrigem world) (colocarDisco(world-pinoDestino world) (world-pinoMeio world)) (world-pinoDestino world)) "destino")]
       )
     ] 
     ))
 
-
-(define (colocarPino listaDiscoOrigem listaDiscoDestino)
+;coloca o disco da lista de origem na lista de Destino; 
+;função helper de moverDisco do pino1 para o pino2
+;listDeDiscos listaDeDiscos -> listaDeDiscos
+(define (colocarDisco listaDiscoOrigem listaDiscoDestino)
   (cond 
     [(empty? listaDiscoOrigem) listaDiscoDestino]
     [(empty? (rest listaDiscoOrigem)) (append listaDiscoDestino (list (first listaDiscoOrigem)) )]
-    [else (colocarPino (rest listaDiscoOrigem) listaDiscoDestino)]
+    [else (colocarDisco(rest listaDiscoOrigem) listaDiscoDestino)]
   )  
 )
+
+;retira os último disco da lista "listaDisco"
 ;listaDeDiscos -> listaDeDiscos
-(define (tirarPinoHelper listaDisco)
+(define (tirarDiscoHelper  listaDisco)
   (cond
    [(empty? listaDisco) empty]
    [else (cond
            [(empty? (rest listaDisco)) empty]  
-           [else (cons (first listaDisco) (tirarPinoHelper (rest listaDisco))) ]
+           [else (cons (first listaDisco) (tirarDiscoHelper  (rest listaDisco))) ]
  ) ]
  )
   )
 
+;retira um disco do pino "pino" da lista de discos de world
 ;world -> world
-(define (tirarPino world pino )
+(define (tirarDisco world pino )
   (cond
-     [(string=? pino "origem") (make-world (world-solucao world) (tirarPinoHelper (world-pinoOrigem world)) (world-pinoMeio world) (world-pinoDestino world))]
-     [(string=? pino "meio")(make-world  (world-solucao world) (world-pinoOrigem world) (tirarPinoHelper (world-pinoMeio world)) (world-pinoDestino world))]
-     [(string=? pino "destino") (make-world (world-solucao world) (world-pinoOrigem world)  (world-pinoMeio world) (tirarPinoHelper (world-pinoDestino world)))]
+     [(string=? pino "origem") (make-world (world-solucao world) (tirarDiscoHelper  (world-pinoOrigem world)) (world-pinoMeio world) (world-pinoDestino world))]
+     [(string=? pino "meio")(make-world  (world-solucao world) (world-pinoOrigem world) (tirarDiscoHelper  (world-pinoMeio world)) (world-pinoDestino world))]
+     [(string=? pino "destino") (make-world (world-solucao world) (world-pinoOrigem world)  (world-pinoMeio world) (tirarDiscoHelper  (world-pinoDestino world)))]
   )
  
  )
@@ -211,7 +228,7 @@
        [(empty? (world-solucao world)) world]
          
        [else
-           (removerMovimentoDalistaSolucao (moverPino world (getOrigem world) (getDestino world))) 
+           (removerMovimentoDalistaSolucao (moverDisco  world (getOrigem world) (getDestino world))) 
        ] 
      )
     
